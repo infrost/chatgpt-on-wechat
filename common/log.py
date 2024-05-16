d2ebc6f49f793d34d2de566fbcd9af79227e1fb6
Frost <hx.zeng@hotmail.com>
2024-05-17 00:46:17 +0800

    Add files via upload
    no log file for vercel

--- common/log.py
+++ common/log.py
@@ -16,14 +16,14 @@ def _reset_logger(log):
             datefmt="%Y-%m-%d %H:%M:%S",
         )
     )
-    file_handle = logging.FileHandler("run.log", encoding="utf-8")
-    file_handle.setFormatter(
-        logging.Formatter(
-            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-    )
-    log.addHandler(file_handle)
+#    file_handle = logging.FileHandler("run.log", encoding="utf-8")
+#    file_handle.setFormatter(
+#        logging.Formatter(
+#            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
+#            datefmt="%Y-%m-%d %H:%M:%S",
+#        )
+ #   )
+#    log.addHandler(file_handle)
     log.addHandler(console_handle)
 
 
