5e5ac4148675526472731729b8af1cceab9559fb
Frost <hx.zeng@hotmail.com>
2024-05-17 03:06:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,45 @@ from config import load_config
 # from plugins import *
 import threading
 
+import logging
+import sys
+
+
+def _reset_logger(log):
+    for handler in log.handlers:
+        handler.close()
+        log.removeHandler(handler)
+        del handler
+    log.handlers.clear()
+    log.propagate = False
+    console_handle = logging.StreamHandler(sys.stdout)
+    console_handle.setFormatter(
+        logging.Formatter(
+            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+    )
+#    file_handle = logging.FileHandler("run.log", encoding="utf-8")
+#    file_handle.setFormatter(
+#        logging.Formatter(
+#            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
+#            datefmt="%Y-%m-%d %H:%M:%S",
+#        )
+ #   )
+#    log.addHandler(file_handle)
+    log.addHandler(console_handle)
+
+
+def _get_logger():
+    log = logging.getLogger("log")
+    _reset_logger(log)
+    log.setLevel(logging.INFO)
+    return log
+
+
+# 日志句柄
+logger = _get_logger()
+
 
 def sigterm_handler_wrap(_signo):
     old_handler = signal.getsignal(_signo)
@@ -74,13 +113,10 @@ class handler(BaseHTTPRequestHandler):
             run()
 
             # Sending response
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write('Hello, world!'.encode('utf-8'))
+
         except Exception as e:
             # Handle any exceptions and log them
-            logger.error("An error occurred: {}".format(str(e)))
+            #logger.error("An error occurred: {}".format(str(e)))
 
 if __name__ == "__main__":
     run()
