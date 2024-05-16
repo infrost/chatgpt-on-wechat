92f3aae42b6b93b766dece7e4d98537bb147b78b
Frost <hx.zeng@hotmail.com>
2024-05-17 03:48:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,11 @@ run()
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        pass
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
         
 #if __name__ == "__main__":
 #    run()
