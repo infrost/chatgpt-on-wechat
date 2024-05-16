01039954c518270d2e157b4795cba828d3e42c99
Frost <hx.zeng@hotmail.com>
2024-05-17 03:11:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,47 +9,8 @@ from http.server import BaseHTTPRequestHandler
 from channel import channel_factory
 from common import const
 from config import load_config
-# from plugins import *
+from plugins import *
 import threading
-
-import logging
-import sys
-
-
-def _reset_logger(log):
-    for handler in log.handlers:
-        handler.close()
-        log.removeHandler(handler)
-        del handler
-    log.handlers.clear()
-    log.propagate = False
-    console_handle = logging.StreamHandler(sys.stdout)
-    console_handle.setFormatter(
-        logging.Formatter(
-            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-    )
-#    file_handle = logging.FileHandler("run.log", encoding="utf-8")
-#    file_handle.setFormatter(
-#        logging.Formatter(
-#            "[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s",
-#            datefmt="%Y-%m-%d %H:%M:%S",
-#        )
- #   )
-#    log.addHandler(file_handle)
-    log.addHandler(console_handle)
-
-
-def _get_logger():
-    log = logging.getLogger("log")
-    _reset_logger(log)
-    log.setLevel(logging.INFO)
-    return log
-
-
-# 日志句柄
-logger = _get_logger()
 
 
 def sigterm_handler_wrap(_signo):
@@ -117,8 +78,11 @@ class handler(BaseHTTPRequestHandler):
         except Exception as e:
             # Handle any exceptions and log them
             #logger.error("An error occurred: {}".format(str(e)))
+            pass
+
+run()
+
 
 if __name__ == "__main__":
     run()
 
-run()
